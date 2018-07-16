cbf6b74aae12d3ade1eb3ecd2365a89243baadf9
duza <duza@tut.by>
2018-07-17 00:55:59 +0300

    Add mock data json file and import mock components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
+// @flow
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+import Jumbotron from './Jumbotron';
+import Feed from './Feed';
+import Contact from './Contact';
+import About from './About';
+import data from '../data/data.json';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
